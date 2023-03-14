90162687f3b5b2c2b95ee0cf1bf7c00b14d3bb58
Angelos Anagnostopoulos <angelosanag@protonmail.com>
2023-03-14 23:42:05 +0200

    Databases started, hello worlds ready
    !

---
+++ implementation/databases/devices/tables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS ligma (
+    ligma_id   SERIAL PRIMARY KEY,
+    username    varchar(255)
+);
+
+INSERT INTO ligma VALUES (1, 'tiko');
+INSERT INTO ligma VALUES (2, 'tico');
+INSERT INTO ligma VALUES (3, 'rico');
+
