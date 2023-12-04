fe55f7cc6d6ace84c2956cb91b8bc2179120fce9
Angelos Anagnostopoulos <angelosanag@protonmail.com>
2023-12-04 20:48:11 +0200

    useful commit message here

--- implementation/servers/authentication/go.mod
+++ implementation/servers/authentication/go.mod
@@ -1,8 +1,9 @@
-module test
+module authentication
 
 go 1.18
 
 require (
+	github.com/gorilla/mux v1.8.1
 	github.com/gorilla/sessions v1.2.2
 	github.com/lib/pq v1.10.9
 )
---
+++ implementation/servers/authentication/utils/utils.go
@@ -0,0 +1,6 @@
+package utils
+
+func (m *MockFingerprintAuthenticator) CheckFingerprint(username string) bool {
+	// Placeholder implementation, always returns true
+	return true
+}
